2b7ada3314adc593b2da4a6069b667fb778a37db
MarieDouguet <douguet@et.esiea.fr>
2020-09-08 14:50:20 +0200

    Add Koin + MVVM Architecture

---
+++ app/src/main/java/com/esiea/android4a/injection/ApplicationModule.kt
@@ -0,0 +1,10 @@
+package com.esiea.android4a.injection
+
+import com.esiea.android4a.MainViewModel
+import org.koin.dsl.module
+
+val presentationModule = module {
+    factory { MainViewModel() }
+
+
+}
