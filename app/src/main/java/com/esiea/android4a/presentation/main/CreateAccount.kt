a2e34bb033d1824970294e160ab0d32246e445aa
MarieDouguet <douguet@et.esiea.fr>
2020-12-14 21:38:13 +0100

    Display the create account page

---
+++ app/src/main/java/com/esiea/android4a/presentation/main/CreateAccount.kt
@@ -0,0 +1,13 @@
+package com.esiea.android4a.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.esiea.android4a.R
+
+class CreateAccount : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+
+    }
+}
--- app/src/main/java/com/esiea/android4a/presentation/main/MainActivity.kt
+++ app/src/main/java/com/esiea/android4a/presentation/main/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.esiea.android4a.presentation.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.widget.Button
 import androidx.lifecycle.Observer
 import com.esiea.android4a.R
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
@@ -10,11 +13,20 @@ import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
 
+
     val mainViewModel: MainViewModel by inject()
+
+
+    fun getEmoji(unicode: Int): String {
+        return String(Character.toChars(unicode))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn_account = findViewById(R.id.create_account_button) as Button
+
+
 
         mainViewModel.loginLiveData.observe(this, Observer {
             when (it) {
@@ -23,14 +35,27 @@ class MainActivity : AppCompatActivity() {
                 }
                 LoginError -> {
                     MaterialAlertDialogBuilder(this)
-                        .setTitle("Erreur")
-                        .setMessage("Compte inconnu")
-                        .setPositiveButton("Ok") { dialog, which ->
+                        .setTitle(Html.fromHtml("<b>"+getString(R.string.error)+"</b>"))
+                        .setMessage("You have to create an account to continue ! " + getEmoji(0x1F60A))
+                        .setPositiveButton("Create an account") { dialog, which ->
+                            val intent = Intent(this, CreateAccount::class.java)
+                            startActivity(intent)
+                        }
+                        .setNeutralButton("Cancel") { dialog, which ->
                             dialog.dismiss()
-                        }.show()
+                        }
+                        .show()
+
                 }
             }
+
         })
+
+        btn_account.setOnClickListener {
+            val intent = Intent(this, CreateAccount::class.java)
+            startActivity(intent)
+        }
+
         login_button.setOnClickListener {
             mainViewModel.onClickedLogin(
                 login_edit.text.toString().trim(),
